4db68628f95611b764cac8239eed76d915008eff
Renato Muller Ventura <re.infotec@gmail.com>
2016-08-02 15:23:52 +0000

    2ª aula de gulp, Sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function(){
-    //place code for your default task here
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+ return gulp.src('assets/src/sass/**/*.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('assets/css'));
 });
