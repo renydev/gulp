426ab293cba6ce3de040bb714063af4cff0712e1
Renato Muller Ventura <re.infotec@gmail.com>
2016-08-02 16:39:55 +0000

    3ª aula de gulp, gulp-concat. Concatenar os javascripts e css.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Gulp
+Lembre-se de quando for adicionar algo
+npm install gulp-concat
+usar o --save-dev
+
+para poder alterar o arquivo package
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat');
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
  return gulp.src('assets/src/sass/**/*.scss')
+   .pipe(concat('style.min.css'))
    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
    .pipe(gulp.dest('assets/css'));
 });
