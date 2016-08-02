de58ef59bab1bb9a0a32bd55e35bfdf60b36b9bc
Renato Muller Ventura <re.infotec@gmail.com>
2016-08-02 19:17:46 +0000

    4ª aula de gulp, gulp-uglify, para comprimir javascript.

---
+++ assets/src/js/text.js
@@ -0,0 +1,5 @@
+var app = (function(document){
+    document.getElementById('logo').innerHTML   =   'Gulp';
+    document.getElementById('logodesc').innerHTML   =   'Automate and enhance your workflow';
+    document.getElementById('video').innerHTML   =   'gulp-uglify';
+}) (document);
