1a25c3932720a1a2e34e65363b05b9448e4d9c93
Renato Ventura <re.infotec@gmail.com>
2019-05-28 20:12:45 -0300

    Typescript
    Inclusão de typescript

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-## Gulp
-Lembre-se de quando for adicionar algo
-
-usar o --save-dev
+Gulp
+Lembre-se de quando for adicionar algo npm install usar o --save-dev
 
 para poder alterar o arquivo package
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const { gulp,
-	      watch, 
-				src, 
-				dest, 
-				series, 
-				parallel } = require('gulp')
+	watch, 
+	src, 
+	dest, 
+	series, 
+	parallel } = require('gulp')
 const sass         = require('gulp-sass')
 const uglifycss    = require('gulp-uglifycss')
 const pug          = require('gulp-pug')
@@ -11,6 +11,7 @@ const clear        = require('clear')
 const postcss      = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const del          = require('del')
+const ts = require('gulp-typescript');
 const browserSync  = require('browser-sync');
 // folders
 const origin = 'src/'
@@ -18,85 +19,90 @@ const output = 'dist/'
 const server = browserSync.create();
 
 function clean(cb) {
-	clear()
-	// del.sync([origin + 'dist'])
-  console.log('Clearing terminal.')
-  cb()
+clear()
+// del.sync([origin + 'dist'])
+console.log('Clearing terminal.')
+cb()
 }
 
 
 function html() {
-	// body omitted
-	console.log('Puggifying HTML')
-	return src(origin + 'html/**')
-	      .pipe(pug())
-	      .pipe(dest(output))
+// body omitted
+console.log('Puggifying HTML')
+return src(origin + 'html/*.pug')
+	.pipe(pug())
+	.pipe(dest(output))
 }
 function css(done) {
-	// body omitted
-	console.log('Sass Transpile')
-	return src(origin + 'styles/sass/**/*.sass')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(postcss([
-      autoprefixer(
-				{ 
-					browsers: [
-						'last 2 versions', 
-						'> 2%',
-						'firefox > = 4',
-						'safari 7 ',
-						'safari 8 ',
-						'IE 8 ',
-						'IE 9 ',
-						'IE 10 ',
-						'IE 11 ',
-					] 
-				}
-		)]))
-		.pipe(uglifycss({"maxLineLen": 80,"uglyComments": true}))
-		.pipe(dest(output + 'css'))
+// body omitted
+console.log('Sass Transpile')
+return src(origin + 'styles/sass/**/*.sass')
+.pipe(sass().on('error', sass.logError))
+.pipe(postcss([
+autoprefixer(
+	{ 
+		browsers: [
+			'last 2 versions', 
+			'> 2%',
+			'firefox >= 4',
+			'safari 7 ',
+			'safari 8 ',
+			'IE 8 ',
+			'IE 9 ',
+			'IE 10 ',
+			'IE 11 ',
+		] 
+	}
+)]))
+.pipe(uglifycss({"maxLineLen": 80,"uglyComments": true}))
+.pipe(dest(output + 'css'))
 
 }
 function typescript(done) {
-	// body omitted
-	console.log('jsTranspile')
-	done()
+// body omitted
+console.log('jsTranspile')
+return src(origin + 'scripts/typescript/**/*.ts')
+	.pipe(ts({
+		noImplicitAny: true
+	}))
+	.pipe(dest(output + 'scripts'));
+done()
 }
 
 function publish(cb) {
-	// body omitted
-	console.log('publish')
-	cb()
+// body omitted
+console.log('publish')
+cb()
 }
 
 function watch_files() {
 
-  // html changes
-  watch(origin + 'html/**/*', series(html, reload))
+// html changes
+watch(origin + 'html/**/*', series(html, reload))
 
-  // css changes
-  watch(origin + 'styles/**/*', series(css, reload))
+// css changes
+watch(origin + 'styles/**/*', series(css, reload))
 
-  // js changes
-  watch(origin + 'scripts/**/*', series(typescript, reload))
+// js changes
+watch(origin + 'scripts/**/*', series(typescript, reload))
 }
 
 function reload(done) {
-	server.reload();
-	done()
+server.reload();
+done()
 }
 
 function browser_sync() {
-  server.init({
-    server: {
-      baseDir: output
-    }
-  })
+server.init({
+server: {
+baseDir: output
+}
+})
 }
 
 exports.default = parallel(
-	clean,
-	parallel(watch_files, browser_sync), 	 
+clean,
+parallel(watch_files, browser_sync), 	 
 )
 
 exports.clear = clean
